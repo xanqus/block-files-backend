fbbf0f7ddc8e2810d122e9695da607533afef0d8
xanqus <sbk98031816@gmail.com>
2023-01-31 10:40:50 +0900

    init project

---
+++ src/main/java/com/example/flowpre_assignment/FlowPreAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowpre_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowPreAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowPreAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flowpre_assignment/FlowPreAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowpre_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowPreAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
